7ea5ea8f972dd8ea9976a8d999505523729998df
Vladimir Litvinchik <litvinchik.vladimir@gmail.com>
2016-05-11 13:43:33 +0300

    Added sample of custom implementation of IFileDataStore.

---
+++ GroupDocs.Viewer.Samples.CustomFileDataStore/JsonFileDataStore.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using GroupDocs.Viewer.Domain;
+using GroupDocs.Viewer.Helper;
+using Newtonsoft.Json;
+
+namespace GroupDocs.Viewer.Samples.CustomFileDataStore
+{
+    public class JsonFileDataStore : IFileDataStore
+    {
+        private readonly DirectoryInfo _workingDirectory;
+
+        public JsonFileDataStore(DirectoryInfo workingDirectory)
+        {
+            if (workingDirectory == null)
+                throw new ArgumentNullException("workingDirectory");
+            if(!workingDirectory.Exists)
+                throw new DirectoryNotFoundException("Unable to find directory '" + workingDirectory.FullName + "'");
+
+            _workingDirectory = workingDirectory;
+        }
+
+        public FileData GetFileData(FileDescription fileDescription)
+        {
+            if(fileDescription == null)
+                throw new ArgumentNullException("fileDescription");
+
+            var path = GetFilePath(fileDescription);
+            var json = File.ReadAllText(path);
+
+            if (fileDescription.DocumentType == "Words")
+                return JsonConvert.DeserializeObject<WordsFileData>(json);
+
+            return JsonConvert.DeserializeObject<FileData>(json);
+        }
+
+        public void SaveFileData(FileDescription fileDescription, FileData fileData)
+        {
+            if (fileDescription == null)
+                throw new ArgumentNullException("fileDescription");
+            if (fileData == null)
+                throw new ArgumentNullException("fileData");
+
+            var path = GetFilePath(fileDescription);
+            var json = JsonConvert.SerializeObject(fileData);
+
+            var dir = Path.GetDirectoryName(path);
+            if (dir != null && !Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            File.WriteAllText(path, json);
+        }
+
+        private string GetFilePath(FileDescription fileDescription)
+        {
+            string directoryName = fileDescription.Guid;
+
+            const char replacementCharacter = '_';
+            const string fileName = "file-data.json";
+
+            char[] invalidChars = Path.GetInvalidFileNameChars();
+            foreach (char invalidChar in invalidChars)
+                directoryName = directoryName.Replace(invalidChar, replacementCharacter);
+
+            string directoryPath = Path.Combine(_workingDirectory.FullName, directoryName);
+
+            return Path.Combine(directoryPath, fileName);
+        }
+    }
+}
---
+++ GroupDocs.Viewer.Samples.CustomFileDataStore/JsonFileDataStoreTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using GroupDocs.Viewer.Domain;
+using NUnit.Framework;
+
+namespace GroupDocs.Viewer.Samples.CustomFileDataStore
+{
+    [TestFixture]
+    public class JsonFileDataStoreTest
+    {
+        [Test]
+        public void ShouldSaveGetFileData()
+        {
+            const string guid = "document.doc";
+            DirectoryInfo workingDir = new DirectoryInfo("c:\\storage\\");
+
+            JsonFileDataStore jsonFileDataStore = new JsonFileDataStore(workingDir);
+
+            DateTime today = DateTime.Now.Date;
+
+            FileData source = new FileData();
+            source.DateCreated = today;
+            source.DateModified = today;
+            source.PageCount = 1;
+
+            FileDescription fileDescription = new FileDescription(guid);
+
+            jsonFileDataStore.SaveFileData(fileDescription, source);
+
+            FileData extractedFileData = jsonFileDataStore.GetFileData(fileDescription);
+
+            Assert.AreEqual(today, extractedFileData.DateModified);
+            Assert.AreEqual(today, extractedFileData.DateCreated);
+            Assert.AreEqual(1, extractedFileData.PageCount);
+        }
+    }
+}
